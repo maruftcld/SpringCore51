c1b09629a3fd58cade84ae3a492102055eab901a
Maruf <maruf.tcld@gmail.com>
2022-12-03 11:04:12 +0600

    Update Trainee.java

--- src/com/spring/test/Trainee.java
+++ src/com/spring/test/Trainee.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component(value = "t121")
+@Component(value = "t121") 
 public class Trainee {
     //primitive type
     private int id;
